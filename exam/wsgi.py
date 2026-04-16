9ba91b0d59b0ad66e914331e940d4e54a6d17e97
ChandraShekhar-Sahu <144163507+ChandraShekhar-Sahu@users.noreply.github.com>
2026-04-16 20:23:08 +0530

    first commit

--- exam/wsgi.py
+++ exam/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 settings_module = 'exam.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'exam.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','exam.settings')
 
 
 application = get_wsgi_application()
