b21498ece6f9b3048ddf81d07b6f0e8f432bf86a
ChandraShekhar-Sahu <144163507+ChandraShekhar-Sahu@users.noreply.github.com>
2026-04-16 20:11:02 +0530

    first commit

--- exam/settings.py
+++ exam/settings.py
@@ -28,7 +28,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
         "localhost",
@@ -91,7 +91,12 @@ WSGI_APPLICATION = "exam.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
